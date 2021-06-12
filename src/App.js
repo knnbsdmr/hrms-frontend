fec3fd78f54ee59032c0dbb88e0a62bfefa1d6e6
hp <hp@DESKTOP-U5AB141>
2021-06-12 20:45:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Navbar from './layouts/Navbar';
+import DashBoard from './layouts/DashBoard';
+import SideBar from './layouts/SideBar';
+import { Container } from 'semantic-ui-react';
+import Footer from './layouts/Footer';
+import CandidateList from './pages/CandidateList';
+import JobAdvertisement from './pages/JobAdvertisement';
+import Filter from './layouts/Filter';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+      <Container className="main">
+      
+        <DashBoard></DashBoard>
+        
+      </Container>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
